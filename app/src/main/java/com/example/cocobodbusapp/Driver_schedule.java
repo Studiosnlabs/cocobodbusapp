411b763e2b93f4ca7bcbd02db006c733a5be15d1
Kerl Kofi Klu <studiosnlabs@gmail.com>
2022-02-18 11:42:09 +0000

    Completed date manipulation

--- app/src/main/java/com/example/cocobodbusapp/Driver_schedule.java
+++ app/src/main/java/com/example/cocobodbusapp/Driver_schedule.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
@@ -34,7 +36,13 @@ public class Driver_schedule extends AppCompatActivity {
     SwitchMaterial fridayMorning;
     SwitchMaterial fridayEvening;
 
-    public void mondayDate(){
+    TextView mondayDate;
+    TextView tuesdayDate;
+    TextView wednesdayDate;
+    TextView thursdayDate;
+    TextView fridayDate;
+
+    public void mondayDate(TextView mondayDateLabel){
 
         Calendar calendar=Calendar.getInstance();
 
@@ -61,6 +69,7 @@ public class Driver_schedule extends AppCompatActivity {
                 Date date1=calendar.getTime();
                 String dateSunday=simpleDateFormat.format(date1);
                 Log.d(TAG, "mondayDate: "+dateSunday);
+                mondayDateLabel.setText(dateSunday);
 
                 break;
 
@@ -69,6 +78,7 @@ public class Driver_schedule extends AppCompatActivity {
                 Date date2=calendar.getTime();
                 String dateTuesday=simpleDateFormat.format(date2);
                 Log.d(TAG, "mondayDate: "+dateTuesday);
+                mondayDateLabel.setText(dateTuesday);
 
                 break;
 
@@ -77,6 +87,7 @@ public class Driver_schedule extends AppCompatActivity {
                 Date date3=calendar.getTime();
                 String dateWednesday=simpleDateFormat.format(date3);
                 Log.d(TAG, "mondayDate: "+dateWednesday);
+                mondayDateLabel.setText(dateWednesday);
 
                 break;
 
@@ -85,6 +96,7 @@ public class Driver_schedule extends AppCompatActivity {
                 Date date4=calendar.getTime();
                 String dateThursday=simpleDateFormat.format(date4);
                 Log.d(TAG, "mondayDate: "+dateThursday);
+                mondayDateLabel.setText(dateThursday);
 
                 break;
 
@@ -93,6 +105,7 @@ public class Driver_schedule extends AppCompatActivity {
                 Date date5=calendar.getTime();
                 String dateFriday=simpleDateFormat.format(date5);
                 Log.d(TAG, "mondayDate: "+dateFriday);
+                mondayDateLabel.setText(dateFriday);
 
                 break;
 
@@ -101,6 +114,193 @@ public class Driver_schedule extends AppCompatActivity {
                 Date date6=calendar.getTime();
                 String dateSaturday=simpleDateFormat.format(date6);
                 Log.d(TAG, "mondayDate: "+dateSaturday);
+                mondayDateLabel.setText(dateSaturday);
+
+                break;
+
+            case Calendar.MONDAY:
+                Date date7=calendar.getTime();
+                String today5=simpleDateFormat.format(date7);
+                Log.d(TAG, "mondayDate: "+today5);
+                mondayDateLabel.setText(today5);
+
+                break;
+
+
+        }
+
+    }
+
+    public void tuesdayDate(TextView mondayDateLabel){
+
+        Calendar calendar=Calendar.getInstance();
+
+        Date now =new Date();
+        calendar.setTime(now);
+
+
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        int dayNum=calendar.get(Calendar.DAY_OF_WEEK);
+
+
+        //int day=calendar.getFirstDayOfWeek();
+
+        String Monday= Integer.toString(day);
+        Log.d(TAG, "mondayDate: "+Monday);
+        Log.d(TAG, "mondayNum: "+dayNum);
+        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("dd MMMM,yyyy");
+
+        switch(dayNum){
+
+
+            case Calendar.SUNDAY:
+                calendar.add(Calendar.DATE,2);
+                Date date1=calendar.getTime();
+                String dateSunday=simpleDateFormat.format(date1);
+                Log.d(TAG, "mondayDate: "+dateSunday);
+                mondayDateLabel.setText(dateSunday);
+
+                break;
+
+            case Calendar.MONDAY:
+                calendar.add(Calendar.DATE,1);
+                Date date2=calendar.getTime();
+                String dateTuesday=simpleDateFormat.format(date2);
+                Log.d(TAG, "mondayDate: "+dateTuesday);
+                mondayDateLabel.setText(dateTuesday);
+
+                break;
+
+            case Calendar.WEDNESDAY:
+                calendar.add(Calendar.DATE,6);
+                Date date3=calendar.getTime();
+                String dateWednesday=simpleDateFormat.format(date3);
+                Log.d(TAG, "mondayDate: "+dateWednesday);
+                mondayDateLabel.setText(dateWednesday);
+
+                break;
+
+            case Calendar.THURSDAY:
+                calendar.add(Calendar.DATE,5);
+                Date date4=calendar.getTime();
+                String dateThursday=simpleDateFormat.format(date4);
+                Log.d(TAG, "mondayDate: "+dateThursday);
+                mondayDateLabel.setText(dateThursday);
+
+                break;
+
+            case Calendar.FRIDAY:
+                calendar.add(Calendar.DATE,4);
+                Date date5=calendar.getTime();
+                String dateFriday=simpleDateFormat.format(date5);
+                Log.d(TAG, "mondayDate: "+dateFriday);
+                mondayDateLabel.setText(dateFriday);
+
+                break;
+
+            case Calendar.SATURDAY:
+                calendar.add(Calendar.DATE,3);
+                Date date6=calendar.getTime();
+                String dateSaturday=simpleDateFormat.format(date6);
+                Log.d(TAG, "mondayDate: "+dateSaturday);
+                mondayDateLabel.setText(dateSaturday);
+
+                break;
+
+            case Calendar.TUESDAY:
+                Date date7=calendar.getTime();
+                String today5=simpleDateFormat.format(date7);
+                Log.d(TAG, "mondayDate: "+today5);
+                mondayDateLabel.setText(today5);
+
+                break;
+
+
+        }
+
+    }
+
+    public void wednesdayDate(TextView mondayDateLabel){
+
+        Calendar calendar=Calendar.getInstance();
+
+        Date now =new Date();
+        calendar.setTime(now);
+
+
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        int dayNum=calendar.get(Calendar.DAY_OF_WEEK);
+
+
+        //int day=calendar.getFirstDayOfWeek();
+
+        String Monday= Integer.toString(day);
+        Log.d(TAG, "mondayDate: "+Monday);
+        Log.d(TAG, "mondayNum: "+dayNum);
+        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("dd MMMM,yyyy");
+
+        switch(dayNum){
+
+
+            case Calendar.SUNDAY:
+                calendar.add(Calendar.DATE,3);
+                Date date1=calendar.getTime();
+                String dateSunday=simpleDateFormat.format(date1);
+                Log.d(TAG, "mondayDate: "+dateSunday);
+                mondayDateLabel.setText(dateSunday);
+
+                break;
+
+            case Calendar.TUESDAY:
+                calendar.add(Calendar.DATE,1);
+                Date date2=calendar.getTime();
+                String dateTuesday=simpleDateFormat.format(date2);
+                Log.d(TAG, "mondayDate: "+dateTuesday);
+                mondayDateLabel.setText(dateTuesday);
+
+                break;
+
+            case Calendar.MONDAY:
+                calendar.add(Calendar.DATE,2);
+                Date date3=calendar.getTime();
+                String dateWednesday=simpleDateFormat.format(date3);
+                Log.d(TAG, "mondayDate: "+dateWednesday);
+                mondayDateLabel.setText(dateWednesday);
+
+                break;
+
+            case Calendar.THURSDAY:
+                calendar.add(Calendar.DATE,6);
+                Date date4=calendar.getTime();
+                String dateThursday=simpleDateFormat.format(date4);
+                Log.d(TAG, "mondayDate: "+dateThursday);
+                mondayDateLabel.setText(dateThursday);
+
+                break;
+
+            case Calendar.FRIDAY:
+                calendar.add(Calendar.DATE,5);
+                Date date5=calendar.getTime();
+                String dateFriday=simpleDateFormat.format(date5);
+                Log.d(TAG, "mondayDate: "+dateFriday);
+                mondayDateLabel.setText(dateFriday);
+
+                break;
+
+            case Calendar.SATURDAY:
+                calendar.add(Calendar.DATE,4);
+                Date date6=calendar.getTime();
+                String dateSaturday=simpleDateFormat.format(date6);
+                Log.d(TAG, "mondayDate: "+dateSaturday);
+                mondayDateLabel.setText(dateSaturday);
+
+                break;
+
+            case Calendar.WEDNESDAY:
+                Date date7=calendar.getTime();
+                String today5=simpleDateFormat.format(date7);
+                Log.d(TAG, "mondayDate: "+today5);
+                mondayDateLabel.setText(today5);
 
                 break;
 
@@ -110,12 +310,194 @@ public class Driver_schedule extends AppCompatActivity {
     }
 
 
+    public void thursdayDate(TextView mondayDateLabel){
+
+        Calendar calendar=Calendar.getInstance();
+
+        Date now =new Date();
+        calendar.setTime(now);
+
+
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        int dayNum=calendar.get(Calendar.DAY_OF_WEEK);
+
+
+        //int day=calendar.getFirstDayOfWeek();
+
+        String Monday= Integer.toString(day);
+        Log.d(TAG, "mondayDate: "+Monday);
+        Log.d(TAG, "mondayNum: "+dayNum);
+        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("dd MMMM,yyyy");
+
+        switch(dayNum){
+
+
+            case Calendar.SUNDAY:
+                calendar.add(Calendar.DATE,4);
+                Date date1=calendar.getTime();
+                String dateSunday=simpleDateFormat.format(date1);
+                Log.d(TAG, "mondayDate: "+dateSunday);
+                mondayDateLabel.setText(dateSunday);
+
+                break;
+
+            case Calendar.TUESDAY:
+                calendar.add(Calendar.DATE,2);
+                Date date2=calendar.getTime();
+                String dateTuesday=simpleDateFormat.format(date2);
+                Log.d(TAG, "mondayDate: "+dateTuesday);
+                mondayDateLabel.setText(dateTuesday);
+
+                break;
+
+            case Calendar.WEDNESDAY:
+                calendar.add(Calendar.DATE,1);
+                Date date3=calendar.getTime();
+                String dateWednesday=simpleDateFormat.format(date3);
+                Log.d(TAG, "mondayDate: "+dateWednesday);
+                mondayDateLabel.setText(dateWednesday);
+
+                break;
+
+            case Calendar.MONDAY:
+                calendar.add(Calendar.DATE,3);
+                Date date4=calendar.getTime();
+                String dateThursday=simpleDateFormat.format(date4);
+                Log.d(TAG, "mondayDate: "+dateThursday);
+                mondayDateLabel.setText(dateThursday);
+
+                break;
+
+            case Calendar.FRIDAY:
+                calendar.add(Calendar.DATE,6);
+                Date date5=calendar.getTime();
+                String dateFriday=simpleDateFormat.format(date5);
+                Log.d(TAG, "mondayDate: "+dateFriday);
+                mondayDateLabel.setText(dateFriday);
+
+                break;
+
+            case Calendar.SATURDAY:
+                calendar.add(Calendar.DATE,5);
+                Date date6=calendar.getTime();
+                String dateSaturday=simpleDateFormat.format(date6);
+                Log.d(TAG, "mondayDate: "+dateSaturday);
+                mondayDateLabel.setText(dateSaturday);
+
+                break;
+
+            case Calendar.THURSDAY:
+                Date date7=calendar.getTime();
+                String today5=simpleDateFormat.format(date7);
+                Log.d(TAG, "mondayDate: "+today5);
+                mondayDateLabel.setText(today5);
+
+                break;
+
+
+        }
+
+    }
+
+    public void fridayDate(TextView mondayDateLabel){
+
+        Calendar calendar=Calendar.getInstance();
+
+        Date now =new Date();
+        calendar.setTime(now);
+
+
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        int dayNum=calendar.get(Calendar.DAY_OF_WEEK);
+
+
+        //int day=calendar.getFirstDayOfWeek();
+
+        String Monday= Integer.toString(day);
+        Log.d(TAG, "mondayDate: "+Monday);
+        Log.d(TAG, "mondayNum: "+dayNum);
+        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("dd MMMM,yyyy");
+
+        switch(dayNum){
+
+
+            case Calendar.SUNDAY:
+                calendar.add(Calendar.DATE,5);
+                Date date1=calendar.getTime();
+                String dateSunday=simpleDateFormat.format(date1);
+                Log.d(TAG, "mondayDate: "+dateSunday);
+                mondayDateLabel.setText(dateSunday);
+
+                break;
+
+            case Calendar.TUESDAY:
+                calendar.add(Calendar.DATE,3);
+                Date date2=calendar.getTime();
+                String dateTuesday=simpleDateFormat.format(date2);
+                Log.d(TAG, "mondayDate: "+dateTuesday);
+                mondayDateLabel.setText(dateTuesday);
+
+                break;
+
+            case Calendar.WEDNESDAY:
+                calendar.add(Calendar.DATE,2);
+                Date date3=calendar.getTime();
+                String dateWednesday=simpleDateFormat.format(date3);
+                Log.d(TAG, "mondayDate: "+dateWednesday);
+                mondayDateLabel.setText(dateWednesday);
+
+                break;
+
+            case Calendar.THURSDAY:
+                calendar.add(Calendar.DATE,1);
+                Date date4=calendar.getTime();
+                String dateThursday=simpleDateFormat.format(date4);
+                Log.d(TAG, "mondayDate: "+dateThursday);
+                mondayDateLabel.setText(dateThursday);
+
+                break;
+
+            case Calendar.MONDAY:
+                calendar.add(Calendar.DATE,4);
+                Date date5=calendar.getTime();
+                String dateFriday=simpleDateFormat.format(date5);
+                Log.d(TAG, "mondayDate: "+dateFriday);
+                mondayDateLabel.setText(dateFriday);
+
+                break;
+
+            case Calendar.SATURDAY:
+                calendar.add(Calendar.DATE,6);
+                Date date6=calendar.getTime();
+                String dateSaturday=simpleDateFormat.format(date6);
+                Log.d(TAG, "mondayDate: "+dateSaturday);
+                mondayDateLabel.setText(dateSaturday);
+
+                break;
+
+            case Calendar.FRIDAY:
+                Date date7=calendar.getTime();
+                String today5=simpleDateFormat.format(date7);
+                Log.d(TAG, "mondayDate: "+today5);
+                mondayDateLabel.setText(today5);
+
+                break;
+
+        }
+
+    }
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_schedule);
 
-        mondayDate();
+
 
         mondayMorning=findViewById(R.id.mondayMorningSwitch);
         mondayEvening=findViewById(R.id.mondayEveningSwitch);
@@ -127,7 +509,17 @@ public class Driver_schedule extends AppCompatActivity {
         thursdayEvening=findViewById(R.id.thursdayEveningSwitch);
         fridayMorning=findViewById(R.id.fridayMorningSwitch);
         fridayEvening=findViewById(R.id.fridayEveningSwitch);
+        mondayDate=findViewById(R.id.mondayDate);
+        tuesdayDate=findViewById(R.id.tuesdayDate);
+        wednesdayDate=findViewById(R.id.wednesdayDate);
+        thursdayDate=findViewById(R.id.thursdayDate);
+        fridayDate=findViewById(R.id.fridayDate);
 
+        mondayDate(mondayDate);
+        tuesdayDate(tuesdayDate);
+        wednesdayDate(wednesdayDate);
+        thursdayDate(thursdayDate);
+        fridayDate(fridayDate);
 
         mondayMorning.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
